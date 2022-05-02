548c316c2992c22023408de6c955038fa658c54e
dfreita4s <danifp19@gmail.com>
2022-05-02 11:27:37 -0300

    starting

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rap-page-rank
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rap_page_rank/core_test.clj
@@ -0,0 +1,7 @@
+(ns rap-page-rank.core-test
+  (:require [clojure.test :refer :all]
+            [rap-page-rank.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
