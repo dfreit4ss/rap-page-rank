52de8dd89a173143689ea353d31e6b074cdec7c6
dfreita4s <danifp19@gmail.com>
2022-05-02 11:57:08 -0300

    reading txt

--- src/rap_page_rank/core.clj
+++ src/rap_page_rank/core.clj
@@ -1,6 +1,15 @@
 (ns rap-page-rank.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(use 'clojure.java.io)
+
+(defn reading-text-file
+  [text-path]
+
+  (with-open [rdr (reader text-path)]
+    (doseq [line (line-seq rdr)]
+      (if (= line "Tim Maia")
+        (println "Achou o tim maia!")
+        (println "Nao achou o tim maia")))))
+
+(reading-text-file "teste.txt")
+
