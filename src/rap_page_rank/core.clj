bf25e4688b179827153bd317dbd0a4e03139a2b9
dfreita4s <danifp19@gmail.com>
2022-05-03 20:49:22 -0300

    list of artists to regex

--- src/rap_page_rank/core.clj
+++ src/rap_page_rank/core.clj
@@ -2,14 +2,23 @@
 
 (use 'clojure.java.io)
 
-(defn reading-text-file
-  [text-path artists]
-  (println (re-seq #"Tim Maia" (slurp text-path))));; verify if in line have "Tim maia"
 
-;(re-seq #"Tim Maia" (slurp "src/lyrics/Jorge Ben Jor - W Brasil")) ;;fazer uma lista separada dos artistas
+(defn find-artist
+  [text list-of-artist]
+  (if (empty? (list-of-artist))
+    (println "Fim")
+    ((println (re-seq (re-pattern (first (list-of-artist))) (slurp text)))
+     (find-artist text (rest list-of-artist)))))
 
 (defn artists [] (list "Tim Maia" "Sabotage" "Gilberto Gil" "Caetano" "Djavan" "Pepeu" "Alceu Valença" "Moraes" "Elba"
                        "Ed Motta" "Cidade Negra" "Skank" "Jorge Ben Jor"))
 
+(defn reading-text-file
+  [text-path artists]
+  (find-artist (slurp text-path) artists));; verify if in line have artists
 
+;; re-pattern : string -> pattern(regex)
+;; re-seq : find a regex in string
+
+;(re-seq (re-pattern (first (artists)))  (slurp "src/lyrics/Jorge Ben Jor - W Brasil"))
 (reading-text-file "src/lyrics/Jorge Ben Jor - W Brasil" (artists))
