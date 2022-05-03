2221cf4429e5dd7fede4e1866194d87feb3811d2
dfreita4s <danifp19@gmail.com>
2022-05-03 01:03:12 -0300

    finding artist's names in txt

--- src/rap_page_rank/core.clj
+++ src/rap_page_rank/core.clj
@@ -2,14 +2,14 @@
 
 (use 'clojure.java.io)
 
+
 (defn reading-text-file
   [text-path]
+  (let [artists '("Tim maia" "tim maia" "sabotage" "sabota" "Sabotage")]
+    (with-open [rdr (reader text-path)]
+      (doseq [line (line-seq rdr)]
+        (if (clojure.string/includes? line "Tim Maia") ;;verify if in line have Tim maia
+          (println "Achou")
+          (println "Nao achou"))))))
 
-  (with-open [rdr (reader text-path)]
-    (doseq [line (line-seq rdr)]
-      (if (= line "Tim Maia")
-        (println "Achou o tim maia!")
-        (println "Nao achou o tim maia")))))
-
-(reading-text-file "teste.txt")
-
+(reading-text-file "src/rap_page_rank/teste.txt")
