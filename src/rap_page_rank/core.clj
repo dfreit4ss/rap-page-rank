b31b3f31b9cd011347515456180c55dc38effb00
dfreita4s <danifp19@gmail.com>
2022-05-03 15:12:40 -0300

    re-seq and slurp

--- src/rap_page_rank/core.clj
+++ src/rap_page_rank/core.clj
@@ -2,16 +2,11 @@
 
 (use 'clojure.java.io)
 
-
 (defn reading-text-file
   [text-path artists]
-    (with-open [rdr (reader text-path)]
-      (doseq [line (line-seq rdr)]
-        (if (clojure.string/includes? line "Tim Maia") ;;verify if in line have "Tim maia"
-          (println "Achou")
-          (println "Nao achou")))))
+  (println (re-seq #"Tim Maia" (slurp text-path))));; verify if in line have "Tim maia"
 
-(re-seq #"Tim Maia" (slurp "src/lyrics/Jorge Ben Jor - W Brasil")) ;;fazer uma lista separada dos artistas
+;(re-seq #"Tim Maia" (slurp "src/lyrics/Jorge Ben Jor - W Brasil")) ;;fazer uma lista separada dos artistas
 
 (defn artists [] (list "Tim Maia" "Sabotage" "Gilberto Gil" "Caetano" "Djavan" "Pepeu" "Alceu Valença" "Moraes" "Elba"
                        "Ed Motta" "Cidade Negra" "Skank" "Jorge Ben Jor"))
