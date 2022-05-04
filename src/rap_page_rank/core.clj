2f682c2baffff1b490684eb6d50a726a3df73c09
dfreita4s <danifp19@gmail.com>
2022-05-04 14:46:30 -0300

    its getting cuter

--- src/rap_page_rank/core.clj
+++ src/rap_page_rank/core.clj
@@ -8,25 +8,29 @@
                        "Ed Motta" "Cidade Negra" "Skank" "Jorge Ben Jor"))
 (defn lyrics [] (list "src/lyrics/Gabriel Pensador - Festa da Musica Tupiniquim" "src/lyrics/Jorge Ben Jor - W Brasil"))
 
+(defn print-matches
+  [lista-de-matches]
+  (println lista-de-matches))
+
 (defn find-artist
   [text list-of-artist]
-  (if (empty? (rest list-of-artist))
-    ;(reading-text-file ())
-    (println "End of recursion")
-    ((print (first list-of-artist) " -> ")
-     (println (count (re-seq (re-pattern (first list-of-artist)) text)))
+  (if (empty? list-of-artist)
+    (list )
+    ((print-matches (list (first list-of-artist)
+                    (count (re-seq (re-pattern (first list-of-artist)) text))))
      (find-artist text (rest list-of-artist)))))
 
 
-(defn reading-text-file
+(defn reading-lyrics-files
   [text-path artists]
-  (if  (not (and (empty? text-path) (empty? artists)))
+  (if (empty? text-path)
+    (println "End of recursion.")
     ((println "Artist: "(first (str/split (first text-path) #"-")) "Music: "(second (str/split (first text-path) #"-")))
-     (find-artist (slurp (first text-path)) artists))
-  (println "Empty lists")))
+     (find-artist (slurp (first text-path)) artists)
+     (reading-lyrics-files (rest text-path) (artists)))))
 
 ;; re-pattern : string -> pattern(regex)
 ;; re-seq : find a regex in string
 
 ;(re-seq (re-pattern (first (artists)))  (slurp "src/lyrics/Jorge Ben Jor - W Brasil"))
-(reading-text-file (lyrics) (artists))
+(reading-lyrics-files (lyrics) (artists))
--- test/rap_page_rank/core_test.clj
+++ test/rap_page_rank/core_test.clj
@@ -2,10 +2,7 @@
   (:require [clojure.test :refer :all]
             [rap-page-rank.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
 
 (deftest check-artist-test
   (testing "checking if pattern will match a artist"
-    (is ((re-seq (re-pattern (first (list-of-artist))) (slurp text))))))
+    (is (= 7 (count (re-seq (re-pattern "Tim Maia")  "Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia"))))))
