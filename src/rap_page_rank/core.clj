057ad1fdaf3e9452433f7910e8973beb4d349cca
dfreita4s <danifp19@gmail.com>
2022-05-04 10:14:55 -0300

    less problems than before

--- src/rap_page_rank/core.clj
+++ src/rap_page_rank/core.clj
@@ -1,15 +1,18 @@
 (ns rap-page-rank.core)
 
+(require '[clojure.string :as str])
 (use 'clojure.java.io)
 
 
 (defn artists [] (list "Tim Maia" "Sabotage" "Gilberto Gil" "Caetano" "Djavan" "Pepeu" "Alceu Valença" "Moraes" "Elba"
                        "Ed Motta" "Cidade Negra" "Skank" "Jorge Ben Jor"))
+(defn lyrics [] (list "src/lyrics/Gabriel Pensador - Festa da Musica Tupiniquim" "src/lyrics/Jorge Ben Jor - W Brasil"))
 
 (defn find-artist
   [text list-of-artist]
-  (if (empty? list-of-artist)
-    (println "Fim")
+  (if (empty? (rest list-of-artist))
+    ;(reading-text-file ())
+    (println "End of recursion")
     ((print (first list-of-artist) " -> ")
      (println (count (re-seq (re-pattern (first list-of-artist)) text)))
      (find-artist text (rest list-of-artist)))))
@@ -17,10 +20,13 @@
 
 (defn reading-text-file
   [text-path artists]
-  (find-artist (slurp text-path) artists));; verify if in line have artists
+  (if  (not (and (empty? text-path) (empty? artists)))
+    ((println "Artist: "(first (str/split (first text-path) #"-")) "Music: "(second (str/split (first text-path) #"-")))
+     (find-artist (slurp (first text-path)) artists))
+  (println "Empty lists")))
 
 ;; re-pattern : string -> pattern(regex)
 ;; re-seq : find a regex in string
 
 ;(re-seq (re-pattern (first (artists)))  (slurp "src/lyrics/Jorge Ben Jor - W Brasil"))
-(reading-text-file "src/lyrics/Gabriel Pensador - Festa da Musica Tupiniquim" (artists))
+(reading-text-file (lyrics) (artists))
