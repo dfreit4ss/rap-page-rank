74d08000357b259fc0b234e8a3708062b72ae079
dfreita4s <danifp19@gmail.com>
2022-05-05 15:25:24 -0300

    recursion is working(i really hope so)

--- src/rap_page_rank/core.clj
+++ src/rap_page_rank/core.clj
@@ -17,20 +17,8 @@
 
 (defn artists [] (list "Tim Maia" "Sabotage" "Gilberto Gil" "Caetano" "Djavan" "Pepeu" "Alceu Valença" "Moraes" "Elba"
                        "Ed Motta" "Cidade Negra" "Skank" "Jorge Ben Jor"))
-(defn lyrics [] (list "src/lyrics/Gabriel Pensador - Festa da Musica Tupiniquim" "src/lyrics/Jorge Ben Jor - W Brasil"))
+(defn lyrics [] (list "resources/lyrics/Jorge Ben Jor - W Brasil" "resources/lyrics/Gabriel Pensador - Festa da Musica Tupiniquim" ))
 
-(defn print-matches
-  [lista-de-matches]
-  (println lista-de-matches))
-
-#_(defn find-artist
-  [text list-of-artist]
-  (if (empty? list-of-artist)
-    (list );;maybe start to insert in graphs here
-    (do
-      (print-matches (list (first list-of-artist)
-                    (count (re-seq (re-pattern (first list-of-artist)) text)))) ;; or here
-     (find-artist text (rest list-of-artist)))))
 
 (defn find-artist
   "Return a list of matches (artist count)"
@@ -41,20 +29,15 @@
 
 (defn slurp-lyrics-files
   [text-path]
-  (slurp text-path)
-  )
-
-
-
-
+  (slurp text-path))
 
 (defn reading-lyrics-files
-  [text-path artists]
-  (if (empty? text-path)
-    (println "End of recursion.")
-    ((println "Artist: "(first (str/split (first text-path) #"-")) "Music: "(second (str/split (first text-path) #"-")))
-     (find-artist (slurp (first text-path)) artists)
-     (reading-lyrics-files (rest text-path) (artists)))))
+  [lyrics artists]
+  (if (empty? lyrics)
+    (list "End of recursion")
+    (do
+      (find-artist (slurp (first lyrics)) artists)
+      (reading-lyrics-files2 (rest lyrics) artists))))
 
 ;; re-pattern : string -> pattern(regex)
 ;; re-seq : find a regex in string
--- test/rap_page_rank/core_test.clj
+++ test/rap_page_rank/core_test.clj
@@ -5,4 +5,17 @@
 
 (deftest check-artist-test
   (testing "checking if pattern will match a artist"
-    (is (= 7 (count (re-seq (re-pattern "Tim Maia")  "Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia"))))))
+    (is (= 7 (count (re-seq (re-pattern "Tim Maia") "Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia Tim Maia"))))))
+
+
+(deftest find-artist-test
+  (testing "Testing if the function will find all artists of a list"
+    (is (= [["Tim Maia " 1]
+            ["Tim Outro " 1]] (find-artist (slurp "resources/lyrics/Teste - Musica Teste") '("Tim Maia" "Tim Outro"))))))
+
+(deftest reading-lyrics-files-test
+  (testing "Testing the recursion to read all lyrics"
+    (is (= [["Tim Maia" 1] ["Sabotage" 0] ["Gilberto Gil" 1] ["Caetano" 1]
+            ["Djavan" 1] ["Pepeu" 1] ["Alceu Valença" 1] ["Moraes" 1] ["Elba" 1]
+            ["Ed Motta" 1] ["Cidade Negra" 1] ["Skank" 1] ["Jorge Ben Jor" 1]] (reading-lyrics-files '("resources/lyrics/Gabriel Pensador - Festa da Musica Tupiniquim") '("Sabotage" "Gilberto Gil" "Caetano" "Tim Maia" "Djavan" "Pepeu" "Alceu Valença" "Moraes" "Elba"
+                                                                                                                                                                                                                        "Ed Motta" "Cidade Negra" "Skank" "Jorge Ben Jor") )))))
